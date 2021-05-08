61d20575af79164d123da2afc8c0eae3dc11f9c6
Emek Gözlüklü <emekgzlkl@gmail.com>
2021-05-08 13:17:34 +0300

    implement task3 solution

--- jira/api.py
+++ jira/api.py
@@ -28,22 +28,20 @@ class EchoResource(Resource):
 
 
 class IssueTypesResource(Resource):
+
     def get(self):
 
-        try:
-            args = issue_type_parser.parse_args()
-            query = args["jiraUrl"] + "rest/api/2/issuetype"
-        except KeyError:
+        args = issue_type_parser.parse_args()
+        if args["jiraUrl"] is None:
             return JIRA_URL_MISSING
 
+        query = args["jiraUrl"] + "rest/api/2/issuetype"
         res = requests.get(query)
 
         if res.status_code != 200:
             return JIRA_NOT_AVAILABLE
         else:
             res = res.json()
-
-
 
         to_return = list()
         for i in res:
@@ -61,14 +59,58 @@ class IssueTypesResource(Resource):
 
 
 class SubtaskTypeResource(Resource):
-    def get(self):
 
+    def get(self):
         args = subtask_type_parser.parse_args()
-        try:
-            query = args["jiraUrl"] + "rest/api/2/issuetype"
-            p_id = args["projectId"] + "rest/api/2/issuetype"
-        except KeyError:
+
+        if args["jiraUrl"] is None:
             return JIRA_URL_MISSING
+
+        query = args["jiraUrl"] + "rest/api/2/search?jql=project={p_id}" + "&maxResults=9999999"
+        p_id = args["projectId"]
+
+        res = requests.get(query.format(p_id=p_id))
+
+        if res.status_code != 200:
+            return JIRA_NOT_AVAILABLE
+        else:
+            res = res.json()
+
+        issues = res["issues"]
+        if issues is None:
+            return JIRA_URL_MISSING
+
+        to_return = list()
+        for issue in issues:
+
+            if issue["fields"]["issuetype"]["subtask"]:
+                new_iss = {
+                    "id": issue["id"],
+                    "key": issue["key"],
+                    "fields": {
+                        "assignee": {
+                            "name": issue["fields"]["assignee"]["name"] if issue["fields"]["assignee"] else None,
+                            "key": issue["fields"]["assignee"]["key"] if issue["fields"]["assignee"] else None,
+                            "emailAddress": issue["fields"]["assignee"]["emailAddress"] if issue["fields"]["assignee"] else None,
+                            "displayName": issue["fields"]["assignee"]["displayName"] if issue["fields"]["assignee"] else None
+                        } if issue["fields"]["assignee"] else None,
+                        "reporter": {
+                            "name": issue["fields"]["reporter"]["name"],
+                            "key": issue["fields"]["reporter"]["key"],
+                            "emailAddress": issue["fields"]["reporter"]["emailAddress"],
+                            "displayName": issue["fields"]["reporter"]["displayName"]
+                        },
+                        "project": {
+                            "id": issue["fields"]["project"]["id"],
+                            "key": issue["fields"]["project"]["key"],
+                            "name": issue["fields"]["project"]["name"],
+                        }
+                    }
+                }
+                to_return.append(new_iss)
+
+        return {"issues": to_return}
+
 
     def put(self):
         return PUT_NOT_SUPPORTED
@@ -76,5 +118,4 @@ class SubtaskTypeResource(Resource):
 
 api.add_resource(EchoResource, "/echo/<text>")
 api.add_resource(IssueTypesResource, "/issuetypes")
-api.add_resource(SubtaskTypeResource, "/subtasks")
-
+api.add_resource(SubtaskTypeResource, "/issues/subtasks")
