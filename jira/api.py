18796a28c7f90f1d89c5b49363cc809457d47e3a
Emek Gözlüklü <emekgzlkl@gmail.com>
2021-05-08 11:54:18 +0300

    add exception handling

--- jira/api.py
+++ jira/api.py
@@ -1,12 +1,16 @@
 from flask_restful import Api, Resource, reqparse
 from flask import Blueprint, abort
 import requests
+from jira.exceptions import *
 
 bp = Blueprint("api", __name__, url_prefix="/api/v1")
 api = Api(bp)
 
 issue_type_parser = reqparse.RequestParser()
 issue_type_parser.add_argument("jiraUrl", type=str)
+
+subtask_type_parser = reqparse.RequestParser()
+subtask_type_parser.add_argument("jiraUrl", type=str)
 
 JIRA_ENDPOINT = "codemaster.obss.io/jira/"
 
@@ -15,10 +19,46 @@ class EchoResource(Resource):
     def get(self, text):
         return text
 
+    def put(self, text):
+        return PUT_NOT_SUPPORTED
+
 
 class IssueTypesResource(Resource):
     def get(self):
-        args = issue_type_parser.parse_args()
+
+        try:
+            args = issue_type_parser.parse_args()
+            query = args["jiraUrl"] + "rest/api/2/issuetype"
+        except KeyError:
+            return JIRA_URL_MISSING
+
+        res = requests.get(query)
+
+        if res.status_code != 200:
+            return JIRA_NOT_AVAILABLE
+        else:
+            res = res.json()
+
+
+
+        to_return = list()
+        for i in res:
+            to_return.append({
+                "id": i["id"],
+                "description": i["description"],
+                "name": i["name"],
+                "subtask": i["subtask"]
+            })
+
+        return to_return
+
+    def put(self):
+        return PUT_NOT_SUPPORTED
+
+
+class SubtaskTypeResource(Resource):
+    def get(self):
+        args = subtask_type_parser.parse_args()
         query = args["jiraUrl"] + "rest/api/2/issuetype"
 
         res = requests.get(query).json()
@@ -35,5 +75,7 @@ class IssueTypesResource(Resource):
         return to_return
 
 
+
 api.add_resource(EchoResource, "/echo/<text>")
 api.add_resource(IssueTypesResource, "/issuetypes")
+api.add_resource(SubtaskTypeResource, "/subtasks")
---
+++ jira/exceptions.py
@@ -0,0 +1,24 @@
+JIRA_URL_MISSING = {
+    "timestamp": "2020-02-11",
+    "status": 400,
+    "errors": "Required String parameter 'jiraUrl' is not present"
+}
+
+JIRA_NOT_AVAILABLE = {
+    "timestamp": "2020-02-11",
+    "status": 500,
+    "errors": "Jira is not available"
+}
+
+GET_NOT_SUPPORTED = {
+"timestamp": "2020-02-11",
+"status": 405,
+"errors": "Request method 'GET' not supported"
+}
+
+PUT_NOT_SUPPORTED = {
+    "timestamp": "2020-02-11",
+    "status": 405,
+    "errors": "Request method 'PUT' not supported"
+}
+
