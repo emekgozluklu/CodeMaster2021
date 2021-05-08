c4edaf2f02bbea402faa39a561c9326e802c64f0
Emek Gözlüklü <emekgzlkl@gmail.com>
2021-05-08 11:45:54 +0300

    implement task 2

--- jira/api.py
+++ jira/api.py
@@ -1,13 +1,39 @@
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 from flask import Blueprint, abort
+import requests
 
 bp = Blueprint("api", __name__, url_prefix="/api/v1")
 api = Api(bp)
 
-class Task1Resource(Resource):
+issue_type_parser = reqparse.RequestParser()
+issue_type_parser.add_argument("jiraUrl", type=str)
+
+JIRA_ENDPOINT = "codemaster.obss.io/jira/"
+
+
+class EchoResource(Resource):
     def get(self, text):
         return text
 
 
+class IssueTypesResource(Resource):
+    def get(self):
+        args = issue_type_parser.parse_args()
+        query = args["jiraUrl"] + "rest/api/2/issuetype"
 
-api.add_resource(Task1Resource, "/echo/<text>")
+        res = requests.get(query).json()
+
+        to_return = list()
+        for i in res:
+            to_return.append({
+                "id": i["id"],
+                "description": i["description"],
+                "name": i["name"],
+                "subtask": i["subtask"]
+            })
+
+        return to_return
+
+
+api.add_resource(EchoResource, "/echo/<text>")
+api.add_resource(IssueTypesResource, "/issuetypes")
